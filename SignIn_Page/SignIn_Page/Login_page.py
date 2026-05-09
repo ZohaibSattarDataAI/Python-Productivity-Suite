45006cb691f2253f85064cbb2d15b5377365c0f6
Zohaib Sattar <zabizubi86@gmail.com>
2026-05-09 11:43:27 +0500

    login page in python

---
+++ SignIn_Page/SignIn_Page/Login_page.py
@@ -0,0 +1,18 @@
+#ZohaibSattarDataAI
+
+email = input("please enter your email=")
+password = int(input("please enter your password="))
+
+#stored data
+
+s_email = 'zabi@gmail.com'
+s_password = 12345
+
+#check conditions 
+
+if email == s_email and password == s_password:
+    print("sucessfull login")
+    print("welcome in website")
+    
+else:
+    print('Try again with correct information')
