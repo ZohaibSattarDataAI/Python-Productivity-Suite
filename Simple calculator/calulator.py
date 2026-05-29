782b9ac4a273e26256e622f2d1db1ed2b7bcc37a
Zohaib Sattar <zabizubi86@gmail.com>
2026-05-29 06:20:24 +0500

    simple calculator program number 4

---
+++ Simple calculator/calulator.py
@@ -0,0 +1,9 @@
+a = int(input("please Enter Value of a ="))
+b = int(input("please enter value of b="))
+
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("reminder",a//b)
+print("Power",a**b)
